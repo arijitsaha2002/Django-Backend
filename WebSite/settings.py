2029f8e8e57480914355cb77924fe6c6bf35cbb4
Arijit Saha <arijitsaha102002@gmail.com>
2025-01-21 23:42:41 +0530

    Local Host and Requirement added

--- WebSite/settings.py
+++ WebSite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i%jb0(mw=u%rgyl+fqncec@kw#n%@5=4to@fs_+(tt*pu*%ggy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.101"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
